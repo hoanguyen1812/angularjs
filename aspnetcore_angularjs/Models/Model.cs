1d07b78dcac0340bda51877bb42df8b21b530846
Nguyen Hoa <nguyenvoanhoa.it@gmail.com>
2019-06-05 09:29:55 +0700

    Update angular project

---
+++ aspnetcore_angularjs/Models/Model.cs
@@ -0,0 +1,10 @@
+namespace aspnetcore_angularjs.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int MakeId { get; set; }
+    }
+}
